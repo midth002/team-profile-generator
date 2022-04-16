d1909cf8d70a79ab20fceca9b77569fa4c9bb2ed
Andrew Midthun <midth002@gmail.com>
2022-04-16 10:58:05 -0500

    Added inquire modules and set up employee class

---
+++ public/employee.js
@@ -0,0 +1,38 @@
+class Employee {
+
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+       const newName = this.name.trim();
+        if (newName === '') {
+            throw 'Please enter a name'
+        }
+        return newName;
+    }
+
+    getId() {
+        const newId = this.id.trim();
+        if (newId === '') {
+            throw 'Please enter an ID'
+        } 
+        return newId;
+    }
+
+    getEmail() {
+        const newEmail = this.email.trim();
+        if (newEmail === '') {
+            throw 'Provide an email'
+        }
+        return newEmail;
+    }
+
+    getRole() {
+        return 'Employee';
+    }
+
+
+}
